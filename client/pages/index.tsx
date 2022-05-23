692c8a79d820a0e8f12d58671d58fa1c046ebcb3
paritomarrr <pari2019cse083abesit@gmail.com>
2022-05-23 19:46:02 +0530

    first commit

---
+++ client/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+ 
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`
+}
+
+const Home: NextPage = () => {
+  return (
+    <div className={style.wrapper}>
+   
+      <h2>Main</h2>
+      <h2>Transaction History</h2>
+    </div>
+  )
+}
+
+export default Home
